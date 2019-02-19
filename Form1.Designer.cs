3d8a400422a9c1f715a658a3fc28a3b5de0ff9f1
Ricardo Felix <ricardofelix2@yahoo.com>
2019-02-19 11:36:27 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace FIleEndings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(463, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SelectFolder);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 102);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(554, 347);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(4, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Discord";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ShowDiscordCache);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(4, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clear All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ClearAllFiles);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(85, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Clear Last";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ClearLastFile);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(564, 426);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(234, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ".pdf",
+            ".png",
+            ".jpg",
+            ".mp4",
+            ".mp3",
+            ".wav",
+            ".docx",
+            ".pptx",
+            ".txt",
+            ".gif",
+            ".ico"});
+            this.comboBox1.Location = new System.Drawing.Point(564, 116);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(393, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "File";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.SelectFile);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(562, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Select Extension / Type Extension";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(691, 114);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(107, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Change Extension";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Begin);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(564, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Select a folder of files or an individual file";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(554, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Select discord for discord cached image files";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(596, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Enter or select an extension";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(581, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Press Change Extension to Begin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Extension Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace FIleEndings
+{
+    public partial class Form1 : Form {
+	    private string path;
+		private List<string> files = new List<string>();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void SelectFolder(object sender, EventArgs e) {
+			files.Clear();
+			FolderBrowserDialog dialog = new FolderBrowserDialog() {
+				Description = @"Select the folder with files for changing"
+			};
+
+			var result = dialog.ShowDialog();
+
+			if (result == DialogResult.OK) {
+				path = dialog.SelectedPath;
+				textBox1.Text = path;
+
+				foreach (var f in Directory.GetFiles(path)) {
+					files.Add(f);
+				}
+
+				ShowFiles();
+            }
+
+        }
+
+        private void ShowFiles() {
+	        if (files.Count < 1) {
+				textBox2.Clear();
+				return;
+	        }
+
+			textBox2.Clear();
+            var paths = files.ToArray();
+            textBox2.Lines = paths;
+        }
+
+        private void ChangeFileExtension(string path, string extension) {
+            if (!extension.StartsWith(".")) extension.Insert(0, ".");
+            
+	        File.Move(path, Path.ChangeExtension(path, extension));
+        }
+
+        private void ClearLastFile(object sender, EventArgs e) {
+	        if (files.Count < 1) return;
+	        files.RemoveAt(files.Count - 1);
+			ShowFiles();
+        }
+
+        private void ClearAllFiles(object sender, EventArgs e) {
+	        files.Clear();
+			ShowFiles();
+        }
+
+        private void ShowDiscordCache(object sender, EventArgs e) {
+	        files.Clear();
+	        if (!Directory.Exists(@"C:\Users\" + Environment.UserName + @"\AppData\Roaming\discord\Cache")) return;
+	        textBox1.Text = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\discord\Cache";
+
+	        foreach (var f in Directory.GetFiles(@"C:\Users\" + Environment.UserName + @"\AppData\Roaming\discord\Cache")) {
+		        files.Add(f);
+	        }
+	        ShowFiles();
+        }
+
+        private void SelectFile(object sender, EventArgs e) {
+	        OpenFileDialog dialog = new OpenFileDialog() {
+		        Title = @"Select File(s) to change extension",
+		        CheckFileExists = true,
+		        Multiselect = true
+	        };
+
+	        var result = dialog.ShowDialog();
+
+	        if (result == DialogResult.OK) {
+		        var names = dialog.FileNames;
+		        
+		        foreach (var f in names) {
+			        if (files.Contains(f)) continue;
+
+			        files.Add(f);
+		        }
+
+		        ShowFiles();
+	        }
+        }
+
+        private void Begin(object sender, EventArgs e) {
+
+	        var extension = comboBox1.Text.Trim();
+
+	        if (files.Count < 1) {
+		        MessageBox.Show("Please select a file.");
+				return;
+	        }
+
+	        var result = DialogResult;
+
+            if (extension.Length < 1)
+		        result = MessageBox.Show("Are you sure you wish to remove all extensions?", "Continue", MessageBoxButtons.YesNo);
+            else
+		        result = MessageBox.Show("Change all files to " + extension + "?", "Continue", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes) {
+	            var percentage = (files.Count / files.Count) * 100;
+	            int i = 0;
+                bool success = true;
+                foreach (var file in files) {
+	                try {
+		                ChangeFileExtension(file, extension);
+	                }
+	                catch (Exception ex) {
+		                result = MessageBox.Show("Error trying to change " + file + Environment.NewLine + ex.Message, "Move on?", MessageBoxButtons.YesNo);
+		                if (result == DialogResult.Yes) continue;
+
+		                else {
+                            success = false;
+                            progressBar1.Value = 0;
+                            return;
+		                }
+	                }
+                    i++;
+                    percentage = (i / files.Count) * 100;
+	                progressBar1.Value = percentage;
+                }
+
+                if(success) MessageBox.Show("Successfully changed all file extensions");
+                progressBar1.Value = 0;
+            }
+        }
+    }
+}
